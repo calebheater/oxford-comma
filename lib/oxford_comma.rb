1da0294e8660e58f44b6e7e70e0cdb602c9dca58
Caleb <calebheater@gmail.com>
2020-12-08 22:15:30 -0500

    commit tree ebeddae762cbec54782497c4dbb4e2b4a175cbb8

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.size >= 1 && array.size < 3
+        array.join(' and ')
+    else
+        final = array.pop
+        array.push('and '+final)
+        array.join(", ")
+    end
 end
